e46ccedc9a35fdf6dd8bde24f0339fabd1138258
osh-een <oisinbell09@gmail.com>
2025-02-15 18:27:06 +0000

    changed dateIncurred in Expense DTO to a Date

--- src/main/java/com/dkit/oop/sd2/DTOs/Expense.java
+++ src/main/java/com/dkit/oop/sd2/DTOs/Expense.java
@@ -1,16 +1,32 @@
 package com.dkit.oop.sd2.DTOs;
 
+import java.sql.Date;
+
+/**                                                     OOP Feb 2022
+ *  Data Transfer Object (DTO)
+ *
+ * This POJO (Plain Old Java Object) is called the Data Transfer Object (DTO).
+ * (or, alternatively, the Model Object or the Value Object).
+ * It is used to transfer data between the DAO and the Business Objects.
+ * Here, it represents a row of data from the User database table.
+ * The DAO creates and populates a User object (DTO) with data retrieved from
+ * the resultSet and passes the User object to the Business Layer.
+ *
+ * Collections of DTOs( e.g. ArrayList<User> ) may also be passed
+ * between the Data Access Layer (DAOs) and the Business Layer objects.
+ */
+
 public class Expense {
     private int expenseID;
     private String title;
     private String category;
     private double amount;
-    private String dateIncurred;
+    private Date dateIncurred;
 
     public Expense() {
     }
 
-    public Expense(int expenseID, String title, String category, double amount, String dateIncurred) {
+    public Expense(int expenseID, String title, String category, double amount, Date dateIncurred) {
         this.expenseID = expenseID;
         this.title = title;
         this.category = category;
@@ -50,11 +66,11 @@ public class Expense {
         this.amount = amount;
     }
 
-    public String getdateIncurred() {
+    public Date getdateIncurred() {
         return dateIncurred;
     }
 
-    public void setdateIncurred(String dateIncurred) {
+    public void setdateIncurred(Date dateIncurred) {
         this.dateIncurred = dateIncurred;
     }
 
